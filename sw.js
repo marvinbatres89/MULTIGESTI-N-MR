4627147f61752b1c4866fee66eb4403a0716f0fa
marvinbatres89 <marvinbatres17@gmail.com>
2026-09-03 22:49:23 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='multigestion-mr-v1-2-0';
-const CORE=['./index.html?v=120','./style.css?v=120','./app.js?v=120','./manifest.json?v=120'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const url=new URL(e.request.url);if(url.origin!==location.origin)return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put('./index.html?v=120',copy)).catch(()=>{});return resp}).catch(()=>caches.match('./index.html?v=120')));return;}e.respondWith(fetch(e.request,{cache:'no-store'}).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return resp}).catch(()=>caches.match(e.request)));});
